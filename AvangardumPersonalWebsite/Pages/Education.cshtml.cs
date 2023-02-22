01ca4483c5b12191f312dd7c5a43de3c9d069c5b
Avangardum <yurykuz65@gmail.com>
2023-02-22 18:11:38 +0300

    Added education page

---
+++ AvangardumPersonalWebsite/Pages/Education.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AvangardumPersonalWebsite.Pages;
+
+public class Education : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
