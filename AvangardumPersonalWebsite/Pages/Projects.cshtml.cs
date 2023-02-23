f6b9b1770196ae8405ffd572d68101df3349c087
Avangardum <yurykuz65@gmail.com>
2023-02-23 14:22:55 +0300

    Added some projects

---
+++ AvangardumPersonalWebsite/Pages/Projects.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AvangardumPersonalWebsite.Pages;
+
+public class Projects : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
