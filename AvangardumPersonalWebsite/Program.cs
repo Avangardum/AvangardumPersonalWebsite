5970abfca86b37fdaf4630f3c1097820d064ca33
Avangardum <yurykuz65@gmail.com>
2023-02-28 17:57:01 +0300

    Added forwarded headers

--- AvangardumPersonalWebsite/Program.cs
+++ AvangardumPersonalWebsite/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseForwardedHeaders();
 app.UseStatusCodePagesWithRedirects("/{0}");
 
 if (!app.Environment.IsDevelopment())
